50af07a6445c8263a67a695d24655e662935238e
Josip Brnas <147553144+Jole56@users.noreply.github.com>
2023-11-11 17:05:53 +0100

    Update carpazar_db.sql

--- carpazar_db.sql
+++ carpazar_db.sql
@@ -1,177 +1,69 @@
--- phpMyAdmin SQL Dump
--- version 5.1.1
--- https://www.phpmyadmin.net/
---
--- Host: 127.0.0.1:3306
--- Generation Time: Nov 06, 2023 at 04:04 PM
--- Server version: 5.7.36
--- PHP Version: 7.4.26
 
-SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-START TRANSACTION;
-SET time_zone = "+00:00";
+CREATE TABLE user(
+	id varchar(36) NOT NULL,
+    user_rating varchar(255),
+    user_name varchar(255) NOT NULL,
+    email varchar(255) NOT NULL,
+    phone_number int NOT NULL,
+    is_premium bool,
+    PRIMARY KEY (id)
+);
 
+CREATE TABLE listing(
+	id varchar(36),
+    user_id varchar(36) NOT NULL,
+    category varchar(255) NOT NULL,
+	date_of_post date NOT NULL,
+    title varchar(255) NOT NULL,
+    description varchar(255) NOT NULL,
+    price int NOT NULL,
+    is_sponsored bool,
+    is_sold int NOT NULL,
+    PRIMARY KEY (id),
+    FOREIGN KEY (user_id) REFERENCES user(id)
+);
 
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
+CREATE TABLE specification(
+	id varchar(36),
+    brand varchar(255) NOT NULL,
+    model varchar(255) NOT NULL,
+    engine_power int NOT NULL,
+    type_of_engine varchar(255)NOT NULL,
+    shifter varchar(255) NOT NULL,
+    kilometers_traveled int NOT NULL,
+    year_of_manufacture year NOT NULL,
+    in_traffic_since datetime,
+    number_of_doors int,
+    location varchar(255) NOT NULL,
+    registration_until date NOT NULL,
+    color varchar(255),
+    owner_num varchar(255),
+    extra_features varchar(255),
+    PRIMARY KEY (id)
+);
 
---
--- Database: `dump1`
---
+CREATE TABLE review(
+	id varchar(36),
+    review varchar(255),
+    user_id varchar(36),
+    PRIMARY KEY (id),
+    FOREIGN KEY (user_id) REFERENCES user(id)
+);
 
--- --------------------------------------------------------
+CREATE TABLE chat(
+	id varchar(36),
+    listing_id varchar(36) NOT NULL,
+    buyer_id varchar(36) NOT NULL,
+    PRIMARY KEY (id),
+    FOREIGN KEY (listing_id) REFERENCES listing(id),
+	FOREIGN KEY (buyer_id) REFERENCES user(id)
+);
 
---
--- Table structure for table `chat`
---
-
-DROP TABLE IF EXISTS `chat`;
-CREATE TABLE IF NOT EXISTS `chat` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `listing_id` int(11) NOT NULL,
-  `buyer_id` int(11) NOT NULL,
-  `is_sold` tinyint(1) NOT NULL,
-  PRIMARY KEY (`id`),
-  KEY `listing_id` (`listing_id`),
-  KEY `buyer_id` (`buyer_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf16 COLLATE=utf16_bin;
-
--- --------------------------------------------------------
-
---
--- Table structure for table `listing`
---
-
-DROP TABLE IF EXISTS `listing`;
-CREATE TABLE IF NOT EXISTS `listing` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `user_id` int(11) NOT NULL,
-  `listing_datetime` datetime NOT NULL,
-  `title` varchar(64) COLLATE utf16_bin NOT NULL,
-  `description` text COLLATE utf16_bin NOT NULL,
-  `price` decimal(10,0) NOT NULL,
-  `is_sponsored` tinyint(1) NOT NULL,
-  PRIMARY KEY (`id`),
-  KEY `user_id` (`user_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf16 COLLATE=utf16_bin;
-
--- --------------------------------------------------------
-
---
--- Table structure for table `message`
---
-
-DROP TABLE IF EXISTS `message`;
-CREATE TABLE IF NOT EXISTS `message` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `chat_id` int(11) NOT NULL,
-  `message_content` text COLLATE utf16_bin NOT NULL,
-  `message_datetime` datetime NOT NULL,
-  PRIMARY KEY (`id`),
-  KEY `chat_id` (`chat_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf16 COLLATE=utf16_bin;
-
--- --------------------------------------------------------
-
---
--- Table structure for table `review`
---
-
-DROP TABLE IF EXISTS `review`;
-CREATE TABLE IF NOT EXISTS `review` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `review` text COLLATE utf16_bin NOT NULL,
-  `user_id` int(11) NOT NULL,
-  `rating` tinyint(4) NOT NULL,
-  PRIMARY KEY (`id`),
-  KEY `user_id` (`user_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf16 COLLATE=utf16_bin;
-
--- --------------------------------------------------------
-
---
--- Table structure for table `specification`
---
-
-DROP TABLE IF EXISTS `specification`;
-CREATE TABLE IF NOT EXISTS `specification` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `brand` varchar(32) COLLATE utf16_bin NOT NULL,
-  `model` varchar(32) COLLATE utf16_bin NOT NULL,
-  `engine_power` tinyint(4) NOT NULL,
-  `engine_type` varchar(16) COLLATE utf16_bin NOT NULL,
-  `shifter_type` varchar(32) COLLATE utf16_bin NOT NULL,
-  `kilometers_travelled` int(11) NOT NULL,
-  `manufacture_year` year(4) NOT NULL,
-  `in_traffic_since` year(4) DEFAULT NULL,
-  `door_number` tinyint(4) DEFAULT NULL,
-  `gear_count` tinyint(4) DEFAULT NULL,
-  `location` varchar(64) COLLATE utf16_bin NOT NULL,
-  `body_shape` varchar(16) COLLATE utf16_bin DEFAULT NULL,
-  `is_used` tinyint(1) NOT NULL,
-  `drive_type` varchar(32) COLLATE utf16_bin DEFAULT NULL,
-  `consumption` decimal(10,0) DEFAULT NULL,
-  `ac_type` varchar(32) COLLATE utf16_bin DEFAULT NULL,
-  `seat_count` tinyint(4) DEFAULT NULL,
-  `registration_until` date DEFAULT NULL,
-  `owner_no` tinyint(4) NOT NULL,
-  `color` varchar(16) COLLATE utf16_bin DEFAULT NULL,
-  `additional_equipment` int(11) DEFAULT NULL,
-  `extra_features` int(11) DEFAULT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf16 COLLATE=utf16_bin;
-
--- --------------------------------------------------------
-
---
--- Table structure for table `user`
---
-
-DROP TABLE IF EXISTS `user`;
-CREATE TABLE IF NOT EXISTS `user` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `user_rating` tinyint(4) DEFAULT NULL,
-  `user_name` varchar(32) COLLATE utf16_bin NOT NULL,
-  `email` varchar(64) COLLATE utf16_bin NOT NULL,
-  `phone_number` varchar(16) COLLATE utf16_bin DEFAULT NULL,
-  `is_premium` tinyint(1) NOT NULL,
-  `full_name` varchar(64) COLLATE utf16_bin NOT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf16 COLLATE=utf16_bin;
-
---
--- Constraints for dumped tables
---
-
---
--- Constraints for table `chat`
---
-ALTER TABLE `chat`
-  ADD CONSTRAINT `chat_ibfk_1` FOREIGN KEY (`listing_id`) REFERENCES `listing` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
-  ADD CONSTRAINT `chat_ibfk_2` FOREIGN KEY (`buyer_id`) REFERENCES `user` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
-
---
--- Constraints for table `listing`
---
-ALTER TABLE `listing`
-  ADD CONSTRAINT `listing_ibfk_1` FOREIGN KEY (`id`) REFERENCES `specification` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
-  ADD CONSTRAINT `listing_ibfk_2` FOREIGN KEY (`user_id`) REFERENCES `user` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
-
---
--- Constraints for table `message`
---
-ALTER TABLE `message`
-  ADD CONSTRAINT `message_ibfk_1` FOREIGN KEY (`chat_id`) REFERENCES `chat` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
-
---
--- Constraints for table `review`
---
-ALTER TABLE `review`
-  ADD CONSTRAINT `review_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `user` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
-COMMIT;
-
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+CREATE TABLE message(
+	id varchar(36),
+    chat_id varchar(36) NOT NULL,
+    message_content varchar(255) NOT NULL,
+    message_datetime datetime NOT NULL,
+    PRIMARY KEY (id),
+    FOREIGN KEY (chat_id) REFERENCES chat(id)
+);
