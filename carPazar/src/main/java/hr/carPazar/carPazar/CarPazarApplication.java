618fd3fea7b3a08aee8902560d43073eaa20f15a
Ante Aljinović <antealjinovic111@gmail.com>
2023-11-18 23:36:32 +0100

    Add files via upload

---
+++ carPazar/src/main/java/hr/carPazar/carPazar/CarPazarApplication.java
@@ -0,0 +1,13 @@
+package hr.carPazar.carPazar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarPazarApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CarPazarApplication.class, args);
+	}
+
+}
---
+++ carPazar/src/test/java/hr/carPazar/carPazar/CarPazarApplicationTests.java
@@ -0,0 +1,13 @@
+package hr.carPazar.carPazar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarPazarApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
