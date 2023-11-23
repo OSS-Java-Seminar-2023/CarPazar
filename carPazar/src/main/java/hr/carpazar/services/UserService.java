e0cd2566797b1bcb918617a89ce51b14755bba36
aljinovic-ante <antealjinovic111@gmail.com>
2023-11-23 23:00:22 +0100

    poor man's navbar2

--- carPazar/src/main/java/hr/carpazar/services/UserService.java
+++ carPazar/src/main/java/hr/carpazar/services/UserService.java
@@ -1,5 +1,6 @@
 package hr.carpazar.services;
 
+import hr.carpazar.models.User;
 import hr.carpazar.repositories.UserRepository;
 import org.springframework.stereotype.Service;
 import org.springframework.beans.factory.annotation.Autowired;
