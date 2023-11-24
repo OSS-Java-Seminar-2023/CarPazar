3dca2058935e1dd9722a8f8b3b94c0bc29126c04
Ante Kljakovic Gaspic <ante@afktours.hr>
2023-11-24 17:02:34 +0100

    added password comparing, inserting registrations into db

--- carPazar/src/main/java/hr/carpazar/services/UserService.java
+++ carPazar/src/main/java/hr/carpazar/services/UserService.java
@@ -2,14 +2,34 @@ package hr.carpazar.services;
 
 import hr.carpazar.models.User;
 import hr.carpazar.repositories.UserRepository;
+import lombok.NoArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.beans.factory.annotation.Autowired;
+import java.util.Map;
+import java.sql.Date;
 
 
+@NoArgsConstructor
 @Service
 public class UserService {
 
     @Autowired
     private UserRepository userRepository;
+
+    public static User createUser(Map<String, String> userData){
+        User user = User.builder().
+                fullName(userData.get("firstName") + " " + userData.get("surname")).
+                birthDate(Date.valueOf(userData.get("birthDate"))).
+                phoneNumber(userData.get("phoneNumber")).
+                userName(userData.get("username")).
+                email(userData.get("email")).
+                hashedPassword(userData.get("password")).build();
+
+        return user;
+    }
+
+    public void registerUser(User user){
+        userRepository.save(user);
+    }
 
 }
