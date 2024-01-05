40ca5d6b907125a9396a0d483fdafb0f2f5efc90
Ante Kljakovic Gaspic <ante@afktours.hr>
2024-01-05 18:12:15 +0100

    minor additions

--- carPazar/src/main/java/hr/carpazar/dtos/ListingDto.java
+++ carPazar/src/main/java/hr/carpazar/dtos/ListingDto.java
@@ -1,8 +1,17 @@
 package hr.carpazar.dtos;
 
+import hr.carpazar.models.Specification;
 import lombok.Data;
+
+import java.time.LocalDateTime;
 
 @Data
 public class ListingDto {
-
+    private LocalDateTime localDateTime;
+    private String title;
+    private String description;
+    private Long price;
+    private Boolean isSponsored;
+    private Boolean isSold;
+    private Specification specifications;
 }
--- carPazar/src/main/java/hr/carpazar/services/ListingService.java
+++ carPazar/src/main/java/hr/carpazar/services/ListingService.java
@@ -2,10 +2,12 @@ package hr.carpazar.services;
 
 import hr.carpazar.dtos.ListingDto;
 import hr.carpazar.models.Listing;
+import hr.carpazar.models.Specification;
 import hr.carpazar.repositories.ListingRepository;
 import org.springframework.stereotype.Service;
 import org.springframework.beans.factory.annotation.Autowired;
 
+import java.time.LocalDateTime;
 import java.util.Hashtable;
 import java.util.Map;
 
@@ -15,7 +17,27 @@ public class ListingService {
 
     public static Listing createListingFromDto(ListingDto listingDto){
         Map<String, String> postParams = new Hashtable<>();
-        return null;
+        postParams.put("localDateTime", listingDto.getLocalDateTime().toString());
+        postParams.put("title", listingDto.getTitle());
+        postParams.put("description", listingDto.getDescription());
+        postParams.put("price", listingDto.getPrice().toString());
+        postParams.put("isSponsored", listingDto.getIsSponsored().toString());
+        postParams.put("isSold", listingDto.getIsSold().toString());
+        postParams.put("specification", listingDto.getSpecifications().toString());
+
+        return createListing(postParams);
+    }
+
+    public static Listing createListing(Map<String, String> listingData){
+        Listing listing = new Listing();
+        listing.setListingDatetime(LocalDateTime.parse(listingData.get("localDateTime")));
+        listing.setTitle(listingData.get("title"));
+        listing.setDescription(listingData.get("description"));
+        listing.setPrice(Long.parseLong(listingData.get("price")));
+        listing.setIsSponsored(Boolean.parseBoolean(listingData.get("isSponsored")));
+        listing.setIsSold(Boolean.parseBoolean(listingData.get("isSold")));
+        // set specs ???
+        return listing;
     }
 
     @Autowired
