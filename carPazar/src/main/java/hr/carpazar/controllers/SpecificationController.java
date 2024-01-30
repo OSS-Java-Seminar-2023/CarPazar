c35a78e4b0f814ee68b796203696ae8fd832c110
aljinovic-ante <antealjinovic111@gmail.com>
2024-01-30 16:33:55 +0100

    edit listing

--- carPazar/src/main/java/hr/carpazar/controllers/SpecificationController.java
+++ carPazar/src/main/java/hr/carpazar/controllers/SpecificationController.java
@@ -15,6 +15,10 @@ import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 import java.util.Optional;
 
 @Controller
@@ -73,17 +77,85 @@ public class SpecificationController {
             specificationDto.setConsumption(specification.getConsumption());
             specificationDto.setAcType(specification.getAcType());
             specificationDto.setSeatCount(specification.getSeatCount());
-            specificationDto.setRegistrationUntil(specification.getRegistrationUntil());
+            LocalDate registrationUntil = specification.getRegistrationUntil();
+            if (registrationUntil != null) {
+                String formattedDate = registrationUntil.format(DateTimeFormatter.ISO_LOCAL_DATE);
+                specificationDto.setRegistrationUntil(LocalDate.parse(formattedDate));
+            }
             specificationDto.setOwnerNo(specification.getOwnerNo());
             specificationDto.setColor(specification.getColor());
-            specificationDto.setAdditionalEquipment(specification.getAdditionalEquipment());
-            specificationDto.setExtraFeatures(specification.getExtraFeatures());
 
             model.addAttribute("specificationDto", specificationDto);
         } else {
             return "redirect:/notFound";
         }
         return "editSpecification";
+    }
+
+    @PostMapping(path="/editSpecification/update")
+    public String updateListing(@ModelAttribute SpecificationDto specificationDto,HttpSession session,Model model) throws ParseException {
+        String loggedInId = (session.getAttribute("user_id") != null) ? session.getAttribute("user_id").toString() : null;
+        if (loggedInId == null) {
+            model.addAttribute("not_logged_in", "You have to log in in order to access this site!");
+            return "notFound";
+        }
+
+        String specId=specificationDto.getId();
+        Optional<Specification> specificationOptional = Optional.ofNullable(specificationService.findByListingId(specId));
+
+
+        if (!specificationOptional.isPresent()) {
+            return "redirect:/notFound";
+        }
+
+        Specification specs=specificationOptional.get();
+        specs.setConsumption(specificationDto.getConsumption());
+        specs.setId(specificationDto.getId());
+        specs.setBrand(specificationDto.getBrand());
+        specs.setModel(specificationDto.getModel());
+        specs.setEngineType(specificationDto.getEngineType());
+        specs.setShifterType(specificationDto.getShifterType());
+        specs.setKilometersTravelled(specificationDto.getKilometersTravelled());
+        specs.setManufactureYear(specificationDto.getManufactureYear());
+        specs.setInTrafficSince(specificationDto.getInTrafficSince());
+        specs.setDoorCount(specificationDto.getDoorCount());
+        specs.setGearCount(specificationDto.getGearCount());
+        specs.setLocation(specificationDto.getLocation());
+        specs.setBodyShape(specificationDto.getBodyShape());
+        specs.setIsUsed(specificationDto.getIsUsed());
+        specs.setDriveType(specificationDto.getDriveType());
+        specs.setConsumption(specificationDto.getConsumption());
+        specs.setAcType(specificationDto.getAcType());
+        specs.setSeatCount(specificationDto.getSeatCount());
+        specs.setRegistrationUntil(specificationDto.getRegistrationUntil());
+        specs.setOwnerNo(specificationDto.getOwnerNo());
+        specs.setColor(specificationDto.getColor());
+
+        specificationService.publishSpecification(specs);
+
+        return "redirect:/mylistings";
+    }
+
+    @GetMapping(path="/editAdditionalEquipment/{listingId}")
+    public String editAdditionalEquipment(@PathVariable String listingId, Model model,HttpSession httpSession){
+        String loggedInId = (httpSession.getAttribute("user_id") != null) ? httpSession.getAttribute("user_id").toString() : null;
+        if (loggedInId == null) {
+            model.addAttribute("not_logged_in", "You have to log in in order to access this site!");
+            return "notFound";
+        }
+        Optional<Specification> specificationOptional = Optional.ofNullable(specificationService.findByListingId(listingId));
+        if (specificationOptional.isPresent()) {
+            Specification specification=specificationOptional.get();
+            SpecificationDto specificationDto = new SpecificationDto();
+            specificationDto.setAdditionalEquipment(specification.getAdditionalEquipment());
+            specificationDto.setExtraFeatures(specification.getExtraFeatures());
+            System.out.println(specification.getAdditionalEquipment());
+            System.out.println(specification.getExtraFeatures());
+            model.addAttribute("specificationDto", specificationDto);
+        } else {
+            return "redirect:/notFound";
+        }
+        return "editAdditionalEquipment";
     }
 
     public SpecificationController(SpecificationService specificationService) {
