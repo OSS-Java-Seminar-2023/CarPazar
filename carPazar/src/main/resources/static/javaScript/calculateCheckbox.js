7f201e877aef50366df5b758b279171cbfb5cf91
Ante Kljakovic Gaspic <ante@afktours.hr>
2024-02-05 09:20:32 +0100

    missing semi colons :O

--- carPazar/src/main/resources/static/javaScript/calculateCheckbox.js
+++ carPazar/src/main/resources/static/javaScript/calculateCheckbox.js
@@ -5,14 +5,16 @@ let equipmentVar = document.getElementsByName("additionalEquipment")[0];
 let extrasVar = document.getElementsByName("extraFeatures")[0];
 
 function onChangeEquip() {
-	let totalEquip = 0 equipmentCheckboxes.forEach(function(checkbox) {
+	let totalEquip = 0;
+	equipmentCheckboxes.forEach(function(checkbox) {
 		if (checkbox.checked) {
 			totalEquip += Math.pow(2, parseInt(checkbox.value));
 		}
 	});
 	equipmentVar.value = totalEquip;
 
-	let totalExtras = 0 extrasCheckboxes.forEach(function(checkbox) {
+	let totalExtras = 0;
+	extrasCheckboxes.forEach(function(checkbox) {
 		if (checkbox.checked) {
 			totalExtras += Math.pow(2, parseInt(checkbox.value));
 		}
